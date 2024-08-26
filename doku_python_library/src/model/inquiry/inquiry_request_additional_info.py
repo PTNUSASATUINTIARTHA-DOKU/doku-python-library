dc80311c2a0a36a3d74b32f2e38354d7e65bc3e6
rafidoku <104545377+rafidoku@users.noreply.github.com>
2024-08-26 11:46:55 +0700

    [FIX] enhance virtualAccountData Model

--- doku_python_library/src/model/inquiry/inquiry_request_additional_info.py
+++ doku_python_library/src/model/inquiry/inquiry_request_additional_info.py
@@ -1,16 +1,15 @@
+from doku_python_library.src.model.va.virtual_account_config import VirtualAccountConfig
 
 class InquiryRequestAdditionalInfo:
 
-    def __init__(self, channel: str,  minAmount: str = None, maxAmount: str = None, trxId: str = None) -> None:
+    def __init__(self, channel: str, trxId: str = None, virtualAccountConfig: VirtualAccountConfig = None) -> None:
         self.channel = channel
-        self.min_amount = minAmount
-        self.max_amount = maxAmount
         self.trx_id = trxId
+        self.virtual_acc_config = virtualAccountConfig
     
     def json(self) -> dict:
         return {
             "channel": self.channel,
-            "minAmount": self.min_amount,
-            "maxAmount": self.max_amount,
-            "trxId": self.trx_id
+            "trxId": self.trx_id,
+            "virtualAccountConfig": self.virtual_acc_config.json()
         }
--- doku_python_library/src/model/inquiry/inquiry_request_virtual_account_data.py
+++ doku_python_library/src/model/inquiry/inquiry_request_virtual_account_data.py
@@ -5,9 +5,10 @@ from doku_python_library.src.model.inquiry.inquiry_request_additional_info impor
 class InquiryRequestVirtualAccountData:
 
     def __init__(self, partnerServiceId: str, customerNo: str, virtualAccountNo: str,
-                 virtualAccountName: str, virtualAccountEmail: str, virtualAccountPhone: str,
+                 virtualAccountName: str, virtualAccountEmail: str,
                  totalAmount: TotalAmount, virtualAccountTrxType: str, expiredDate: str,
-                 additionalInfo: InquiryRequestAdditionalInfo, inquiryStatus: str, inquiryReason: InquiryReason, inquiryRequestId: str, trxId: str) -> None:
+                 additionalInfo: InquiryRequestAdditionalInfo, inquiryStatus: str, inquiryReason: InquiryReason, inquiryRequestId: str, 
+                 trxId: str = None, virtualAccountPhone: str = None) -> None:
         self.partner_service_id = partnerServiceId
         self.customer_no = customerNo
         self.virtual_acc_no = virtualAccountNo
@@ -30,13 +31,13 @@ class InquiryRequestVirtualAccountData:
             "virtualAccountNo": self.virtual_acc_no,
             "virtualAccountName": self.virtual_acc_name,
             "virtualAccountEmail": self.virtual_acc_email,
-            "virtualAccountPhone": self.virtual_acc_phone,
+            "virtualAccountPhone": None if self.virtual_acc_phone is None else self.virtual_acc_phone,
             "totalAmount": self.total_amount.json(),
             "virtualAccountTrxType": self.virtual_acc_trx_type,
             "expiredDate": self.expired_date,
             "additionalInfo": self.additional_info.json(),
             "inquiryStatus": self.inquiry_status,
-            "inquiryReason": self.inquiry_reason,
+            "inquiryReason": self.inquiry_reason.json(),
             "inquiryRequestId": self.inquiry_request_id,
             "trxId": None if self.trx_id is None else self.trx_id
         }
