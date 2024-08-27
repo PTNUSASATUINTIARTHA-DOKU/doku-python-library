637b17630b4a17a5de411b3e7d5f037e2af74d43
rafidoku <104545377+rafidoku@users.noreply.github.com>
2024-08-27 13:21:22 +0700

    add README and set channel_id header default

--- doku_python_library/src/model/general/request_header.py
+++ doku_python_library/src/model/general/request_header.py
@@ -1,6 +1,6 @@
 class RequestHeader:
 
-    def __init__(self, x_timestamp: str, x_signature: str, x_partner_id: str, x_external_id: str, channel_id: str, authorization: str):
+    def __init__(self, x_timestamp: str, x_signature: str, x_partner_id: str, x_external_id: str, authorization: str, channel_id: str="SDK"):
         self.x_timestamp = x_timestamp
         self.x_signature = x_signature
         self.x_partner_id = x_partner_id
