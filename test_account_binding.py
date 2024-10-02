e44e35e5e57fa2861182317519638a8a698d13d7
rafidoku <104545377+rafidoku@users.noreply.github.com>
2024-10-02 13:32:36 +0700

    unit test for account binding

--- test_account_binding.py
+++ test_account_binding.py
@@ -20,10 +20,72 @@ class TestAccountBinding(unittest.TestCase):
         mock_data.assert_called_with(Config.get_base_url(is_production=False)+self.account_binding_url, json=request.json())
         self.assertEqual(response.json().response_code, resp_dict.response_code)
     
-    def test_account_binding_phone_null(self, mock_data):
+    def test_account_binding_phone_null(self):
         request = Util.generate_account_binding_request()
         request.phone_no = None
         with self.assertRaises(Exception) as context:
-            request.validate_va_request()
-        self.assertEqual(str(context.exception.args[0]), "partnerServiceId cannot be null. Please provide a partnerServiceId. Example: ' 888994'.")
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "phoneNo cannot be null. Please provide a phoneNo. Example: '62813941306101'.")
     
+    def test_account_binding_phone_min(self):
+        request = Util.generate_account_binding_request()
+        request.phone_no = "628891"
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "phoneNo must be at least 9 digits. Ensure that phoneNo is not empty. Example: '62813941306101'.")
+    
+    def test_account_binding_phone_max(self):
+        request = Util.generate_account_binding_request()
+        request.phone_no = "12345678909876543"
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "phoneNo must be 16 characters or fewer. Ensure that phoneNo is no longer than 16 characters. Example: '62813941306101'.")
+    
+    def test_account_binding_channel_null(self):
+        request = Util.generate_account_binding_request()
+        request.additional_info.channel = None
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "additionalInfo.channel cannot be null. Ensure that additionalInfo.channel is one of the valid channels. Example: 'DIRECT_DEBIT_ALLO_SNAP'.")
+    
+    def test_account_binding_channel_not_in_enum(self):
+        request = Util.generate_account_binding_request()
+        request.additional_info.channel = "CHANNEL_A"
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "additionalInfo.channel is not valid. Ensure that additionalInfo.channel is one of the valid channels. Example: 'DIRECT_DEBIT_ALLO_SNAP'.")
+    
+    def test_account_binding_cust_id_merchant_null(self):
+        request = Util.generate_account_binding_request()
+        request.additional_info.cust_id_merchant = None
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "additionalInfo.custIdMerchant cannot be null. Please provide a additionalInfo.custIdMerchant. Example: 'cust-001'.")
+    
+    def test_account_binding_cust_id_merchant_max(self):
+        request = Util.generate_account_binding_request()
+        request.additional_info.cust_id_merchant = "12345678901234567890123456789012345678901234567890123456789011234567890123456789012345678901234567890123456789012345678901"
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "additionalInfo.custIdMerchant must be 64 characters or fewer. Ensure that additionalInfo.custIdMerchant is no longer than 16 characters. Example: 'cust-001'.")
+    
+    def test_account_binding_cust_id_merchant_max(self):
+        request = Util.generate_account_binding_request()
+        request.additional_info.cust_id_merchant = "12345678901234567890123456789012345678901234567890123456789011234567890123456789012345678901234567890123456789012345678901"
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "additionalInfo.custIdMerchant must be 64 characters or fewer. Ensure that additionalInfo.custIdMerchant is no longer than 16 characters. Example: 'cust-001'.")
+    
+    def test_account_binding_success_url_null(self):
+        request = Util.generate_account_binding_request()
+        request.additional_info.success_registration_url = None
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "additionalInfo.successRegistrationUrl cannot be null. Please provide a additionalInfo.successRegistrationUrl. Example: 'https://www.doku.com'.")
+    
+    def test_account_binding_failed_url_null(self):
+        request = Util.generate_account_binding_request()
+        request.additional_info.failed_registration_url = None
+        with self.assertRaises(Exception) as context:
+            request.validate_request()
+        self.assertEqual(str(context.exception.args[0]), "additionalInfo.failedRegistrationUrl cannot be null. Please provide a additionalInfo.failedRegistrationUrl. Example: 'https://www.doku.com'.")
